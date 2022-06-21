dbbd9401bf6d868710928c6cda4177bfb502f77d
mdhamed238 <mdhamed0238@gmail.com>
2022-06-21 02:22:08 +0000

    Production build

--- client/src/url/index.js
+++ client/src/url/index.js
@@ -1,1 +1,3 @@
-export default baseUrl = (process.env.NODE_ENV === 'production') ? 'https://dashboard.heroku.com/apps/coolcinema' : 'localhost:5000';
+const baseUrl = (process.env.NODE_ENV === 'production') ? 'https://dashboard.heroku.com/apps/coolcinema' : 'localhost:5000';
+
+export default baseUrl
